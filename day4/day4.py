6147a3a1667e041a3dba7056053100562a022b44
jgonzalezmorente <jgonzalezmorente@gmail.com>
2019-12-26 23:23:06 +0100

    Día 4 - parte 2

--- day4/day4.py
+++ day4/day4.py
@@ -16,24 +16,61 @@ Other than the range rule, the following are true:
 How many different passwords within the range given in your puzzle input meet these criteria?
 
 Your puzzle input is 158126-624574.
+
+--- Part Two ---
+An Elf just remembered one more important detail: the two adjacent matching digits are not part of a larger group of matching digits.
+
+Given this additional criterion, but still ignoring the range rule, the following are now true:
+
+112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
+123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
+How many different passwords within the range given in your puzzle input meet all of the criteria?
+
 """
-
-#%%
-class Password(object):
-    def __init__(self, dig_doble, posicion):
-        self.dig_doble = dig_doble
-        self.posicion = posicion
-        self.passwords = [[self.dig_doble, self.dig_doble]]
+class Passwords(object):
+    def __init__(self, lim_inf, lim_sup):
+        self.lim_inf = lim_inf
+        self.lim_sup = lim_sup
     
-    def generar_parte_superior(self):
-        while(self.posicion <= 3):
-            for password in self.passwords:
-                for nuevo_digito in enumerate(password[-1], 9):
-                    password.append(nuevo_digito)
-            self.posicion+=1
+    def es_creciente(self):
+        for i in range(len(self.p_lista_)-1):
+            if self.p_lista_[i] > self.p_lista_[i+1]:
+                return False
+        return True
+    
+    def tiene_duplicados(self):
+        for i in range(len(self.p_lista_)-1):
+            if self.p_lista_[i] == self.p_lista_[i+1]:
+                return True
+        return False
+       
+    def generar(self):
+        self.passwords_ = []
+        for p in range(self.lim_inf, self.lim_sup + 1):
+            self.p_lista_ = [int(d) for d in str(p)]
+            if self.es_creciente() and self.tiene_duplicados():
+                print(f"Password encontrada: {self.p_lista_}")
+                self.passwords_.append(self.p_lista_)
+    
+    def filtrar_grupos(self):
+        self.passwords_long2_ = []
+        for p in self.passwords_:            
+            for d in set(p):
+                i = p.index(d)
+                if (i + 1) < len(p) and p[i] == p[i+1]:
+                    if (i + 2 == len(p)) or p[i+2] != p[i+1]:
+                        print(f"Password con un grupo de dos dígitos repetidos exactamente encontrado: {p}")
+                        self.passwords_long2_.append(p)
+                        break                  
 
-        
+if __name__ == '__main__':
+    passwords = Passwords(158126, 624574)
+    passwords.generar()
+    print("=======================================")
+    print(f"Número total de passwords encontradas: {len(passwords.passwords_)}" )
 
-
-
-# %%
+    print("============== PARTE 2 ================")
+    passwords.filtrar_grupos()
+    print("=========================================================================================")
+    print(f"Número total de passwords encontradas: {len(passwords.passwords_long2_)}" )
